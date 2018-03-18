da82d64bbfc4cdc39d3179ff424205d63fed53c5
Ronny <diaz.r93@gmail.com>
2018-03-18 19:44:55 -0400

    added Updated and Missing Files

--- src/grid_service/src/AllocatedCell.cpp
+++ src/grid_service/src/AllocatedCell.cpp
@@ -12,10 +12,18 @@ uint8_t * const AllocatedCell::getCellAddress() {
 	return (uint8_t *) this;
 }
 
+float AllocatedCell::getCellStatistic(uint8_t stride) const {
+	return statistic_registers.at(stride);
+}
+
+void AllocatedCell::setCellStatistic(uint8_t stride, float data) {
+	statistic_registers.at(stride) = data;
+}
+
 uint8_t AllocatedCell::getCellStatus(uint8_t stride) const {
 	return status_registers.at(stride);
 }
 
 void AllocatedCell::setCellStatus(uint8_t stride, uint8_t data) {
-	status_registers.at(stride) = data;
+	status_registers.at(stride) ^= data;
 }
--- src/grid_service/src/MappedCell.cpp
+++ src/grid_service/src/MappedCell.cpp
@@ -7,56 +7,34 @@
 #include "MappedCell.h"
 
 // Static Class Variables
-const size_t MappedCell::cell_size = sizeof(uint8_t) * 3;
-const size_t MappedCell::page_size = std::ceil(150 * 150 * cell_size/(float) getpagesize()) * getpagesize();
-const size_t MappedCell::mmap_stride = MappedCell::page_size/MappedCell::cell_size;
-
-uint8_t * MappedCell::mmap_base_addr = (uint8_t *) mmap(
-	NULL,
-	MappedCell::page_size,
-	PROT_READ | PROT_WRITE,
-	MAP_ANONYMOUS | MAP_PRIVATE,
-	-1,
-	0
-);
-uint8_t * MappedCell::mmap_free_addr = MappedCell::mmap_base_addr;
-
 const uint8_t MappedCell::STATUS_REGISTER_OFFSET = 0;
-const uint8_t MappedCell::STATISTIC_REGISTER_OFFSET = 3;
+const uint8_t MappedCell::STATISTIC_REGISTER_OFFSET = 4;
 
 // Public Member Functions
-MappedCell::MappedCell() : 
-	cell_base_addr(MappedCell::mmap_free_addr)
-{
-	MappedCell::mmap_free_addr += MappedCell::cell_size;
-}
+MappedCell::MappedCell(uint8_t * addr) : 
+	cell_base_addr(addr)
+{ }
 
 MappedCell::~MappedCell() {
 
-}
-
-void MappedCell::initializeMap() {
-        if(MappedCell::mmap_base_addr == (uint8_t *) -1)
-                throw std::bad_alloc();
-}
-
-void MappedCell::cleanupMap() {
-	munmap(MappedCell::mmap_base_addr, MappedCell::page_size);
-}
-
-bool MappedCell::isMapFull() {
-        return (MappedCell::mmap_free_addr - MappedCell::mmap_base_addr)/MappedCell::cell_size > MappedCell::mmap_stride;
 }
 
 uint8_t * const MappedCell::getCellAddress() {
 	return this->cell_base_addr;
 }
 
+float MappedCell::getCellStatistic(uint8_t stride) const {
+	return *(((float *) (this->cell_base_addr + MappedCell::STATISTIC_REGISTER_OFFSET)) + stride);
+}
+
+void MappedCell::setCellStatistic(uint8_t stride, float data) {
+	*(((float *) (this->cell_base_addr + MappedCell::STATISTIC_REGISTER_OFFSET)) + stride) = data;
+}
 
 uint8_t MappedCell::getCellStatus(uint8_t stride) const {
 	return *(this->cell_base_addr + MappedCell::STATUS_REGISTER_OFFSET + stride);
 }
 
 void MappedCell::setCellStatus(uint8_t stride, uint8_t data) {
-	*(this->cell_base_addr + MappedCell::STATUS_REGISTER_OFFSET + stride) = data;	
+	*(this->cell_base_addr + MappedCell::STATUS_REGISTER_OFFSET + stride) ^= data;	
 }
